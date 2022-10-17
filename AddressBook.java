8617e277b3250a089063e5017f5c6b886cda83d2
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-10-17 17:28:21 +0530

    Able to Edit Existing contact of a person

--- AddressBook.java
+++ AddressBook.java
@@ -5,71 +5,7 @@ import java.util.Scanner;
 import java.io.*;
 
 public class AddressBook {
-//	static String insertQuery = "insert into contact(firstName, lastName, address,"
-//			+ " cityName, stateName, zip, phoneNumber) values('Niti', 'Singh', 'ganesh nagar','amritsar','Punjab','422204','973447479')";
-	
-//	public static void main(String[] args) throws ClassNotFoundException, SQLException  {
-//		System.out.println("Welcome to Address Book of MySQL");
-//		Scanner sc = new Scanner(System.in);
-//		int input = 0;
-//		try {
-//			// load driver
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//			System.out.println("Driver loaded...");
-//
-//			// make connection
-//			String DBName = "Addressbook";
-//			String URL = "jdbc:mysql://localhost:3306/" + DBName;
-//			String dbUserName = "root";
-//			String dbPass = "Root";
-//			Connection con = DriverManager.getConnection(URL, dbUserName, dbPass);
-//			System.out.println("Connected with " + dbUserName);
-//
-//			// create statement
-//			Statement stat = con.createStatement();
-//			System.out.println("Statement Obj Created...");
-//			do {
-//				System.out.println("1. Insert Query");
-//				System.out.println("2. Select Query");
-//				System.out.println("0. Exit");
-//				System.out.println("Enter Operation You Wanna Perform : ");
-//				input = sc.nextInt();
-//
-//				switch (input) {
-//				case 1:
-//
-//					// execute query
-//					int noOFRowsAffected = stat.executeUpdate(insertQuery);
-//					if (noOFRowsAffected > 0)
-//						System.out.println("Record Inserted!");
-//					break;
-//
-//				case 2:
-//					String selectQuery = "select * from priyanka";
-//					ResultSet rs = stat.executeQuery(selectQuery);
-//					System.out.println(rs);
-//					System.out.println("Id | Name | Password");
-//					while (rs.next()) {
-//						System.out.println(rs.getInt(1) + " | " + rs.getString(2) + " | " + rs.getString(3));
-//					}
-//					break;
-//
-//				case 0:
-//					System.out.println("Exited");
-//					break;
-//
-//				default:
-//					System.out.println("Invalid Input");
-//					break;
-//				}
-//
-//			} while (input != 0);
-//		} catch (ClassNotFoundException e) {
-//			System.out.println(e);
-//		} catch (SQLException e) {
-//			System.out.println(e);
-//		}
-//	}
+
 	public static void main(String[] args) {
 		AddressBook objAddressBook = new AddressBook();
 		System.out.println(objAddressBook);
--- Test.java
+++ Test.java
@@ -2,19 +2,50 @@ package com.bl.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.cj.protocol.Resultset;
 
 public class Test {
 	public static void main(String[] args) {
 		Test objTest = new Test();
-		objTest.create_data( "pri", "Bachhav", "AirForce_camp", "Nashik", "Maharashtra", "422207", "98474582");
-
+		// objTest.create_data( "pri", "Bachhav", "AirForce_camp", "Nashik",
+		// "Maharashtra", "422207", "98474582");
+		// objTest.read_data("pri");
+		objTest.update_data("pri", "Airforce_camp", "Nsk", "MH", "8767", "88888888", "98474582");
 	}
-	public void create_data(String firstName, String lastName, String address,
-			String cityName, String stateName, String zip, String phoneNumber) {
-		
+
+	public void read_data(String firstName) {
+
 		AddressBook objAddressBook = new AddressBook();
 		Connection connection = objAddressBook.get_connection();
-		
+
+		PreparedStatement pa = null;
+		ResultSet rs = null;
+		try {
+			String query = "select * from contact where firstName = ?";
+			pa = connection.prepareStatement(query);
+			pa.setString(1, firstName);
+
+			System.out.println(pa);
+			rs = pa.executeQuery();
+
+			while (rs.next()) {
+				System.out.println("Name : " + rs.getString("firstName"));
+				System.out.println("Last Name : " + rs.getString("lastName"));
+				System.out.println("Contact : " + rs.getString("phoneNumber"));
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void create_data(String firstName, String lastName, String address, String cityName, String stateName,
+			String zip, String phoneNumber) {
+
+		AddressBook objAddressBook = new AddressBook();
+		Connection connection = objAddressBook.get_connection();
+
 		PreparedStatement pa = null;
 		try {
 			String query = "insert into contact(firstName, lastName, address,"
@@ -29,6 +60,32 @@ public class Test {
 			pa.setString(7, phoneNumber);
 			System.out.println(pa);
 			pa.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void update_data(String firstName, String address, String cityName, String stateName, String zip,
+			String phoneNumber, String new_phoneNumber) {
+
+		AddressBook objAddressBook = new AddressBook();
+		Connection connection = objAddressBook.get_connection();
+
+		PreparedStatement pa = null;
+
+		try {
+			String query = "update contact set firstName = ?, address = ?, cityName = ?, stateName = ?, phoneNumber = ? where phoneNumber = ?";
+			pa = connection.prepareStatement(query);
+			pa.setString(1, firstName);
+			pa.setString(2, address);
+			pa.setString(3, cityName);
+			pa.setString(4, stateName);
+			pa.setString(5, phoneNumber);
+			pa.setString(6, new_phoneNumber);
+
+			System.out.println(pa);
+			pa.executeUpdate();
+
 		} catch (Exception e) {
 			System.out.println(e);
 		}
